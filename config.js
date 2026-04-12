3d2e612d0709df990c6cc3e843e4ce03018b79f2
gaga050819 <gaga050819@gmail.com>
2026-04-12 16:15:40 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,1 @@
-const SITE_VERSION = "v1.8";
-const IDEAS_GAS_URL = 'https://script.google.com/macros/s/AKfycbxFg6A2iqRlkGkidiqRnJon6OrljzyFNS-gUE7vGIHAA4JLWWh0GsMJcUwmIR3JCaYl/exec';
-const BUGS_GAS_URL = 'https://script.google.com/macros/s/AKfycbzkeVak2unEL-d9bm90jVTN-Evk5eDP0rzTzVnYNpCZzx_ZXns4YQJYKKMSxghWsw/exec';
-const IDEA_TOKEN = 'Idea_2026'; 
-const BUG_TOKEN = 'Bug_2026';
+const SITE_VERSION='v1.8',IDEAS_GAS_URL='https://script.google.com/macros/s/AKfycbxFg6A2iqRlkGkidiqRnJon6OrljzyFNS-gUE7vGIHAA4JLWWh0GsMJcUwmIR3JCaYl/exec',BUGS_GAS_URL='https://script.google.com/macros/s/AKfycbzkeVak2unEL-d9bm90jVTN-Evk5eDP0rzTzVnYNpCZzx_ZXns4YQJYKKMSxghWsw/exec',IDEA_TOKEN='Idea_2026',BUG_TOKEN='Bug_2026';globalThis['SITE_VERSION']=SITE_VERSION;globalThis['IDEAS_GAS_URL']=IDEAS_GAS_URL;globalThis['BUGS_GAS_URL']=BUGS_GAS_URL;globalThis['IDEA_TOKEN']=IDEA_TOKEN;globalThis['BUG_TOKEN']=BUG_TOKEN;
