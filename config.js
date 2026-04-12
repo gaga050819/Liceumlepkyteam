956ef7e95309b6d205800cf4cf55d42b3e1acd4b
gaga050819 <gaga050819@gmail.com>
2026-04-12 16:11:33 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,5 @@
 const SITE_VERSION = "v1.8";
+const IDEAS_GAS_URL = 'https://script.google.com/macros/s/AKfycbxFg6A2iqRlkGkidiqRnJon6OrljzyFNS-gUE7vGIHAA4JLWWh0GsMJcUwmIR3JCaYl/exec';
+const BUGS_GAS_URL = 'https://script.google.com/macros/s/AKfycbzkeVak2unEL-d9bm90jVTN-Evk5eDP0rzTzVnYNpCZzx_ZXns4YQJYKKMSxghWsw/exec';
+const IDEA_TOKEN = 'Idea_2026'; 
+const BUG_TOKEN = 'Bug_2026';
